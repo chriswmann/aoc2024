6bad9a30542f82e6a233e5a4fd7f97e36955aa08
chriswmann <8133496+chriswmann@users.noreply.github.com>
2024-12-03 19:08:47 +0000

    Add day 01 unit tests

--- src/day01.rs
+++ src/day01.rs
@@ -1,35 +1,37 @@
 use crate::cli::Part;
 pub fn run(data: &str, part: Option<Part>) {
     if let Some(part) = part {
-        match part {
+        let answer = match part {
             Part::One => part01(data),
             Part::Two => part02(data),
-        }
+        };
+        println!("Day 1, Part {} answer is: {}", part, answer);
         return;
     }
-    part01(data);
-    part02(data);
+    let part01_answer = part01(data);
+
+    println!("Day 1, Part 1 answer is: {}", part01_answer);
+    let part02_answer = part02(data);
+    println!("Day 1, Part 2 answer is: {}", part02_answer);
 }
 
-pub fn part01(data: &str) {
+pub fn part01(data: &str) -> u32 {
     let (mut first, mut second) = parse_input(data);
     first.sort_unstable();
     second.sort_unstable();
-    let answer: u32 = first
+    first
         .iter()
         .zip(second.iter())
         .map(|(a, b)| (*a as i32 - *b as i32).unsigned_abs())
-        .sum();
-    println!("Day 1, Part 1 answer is: {}", answer);
+        .sum()
 }
 
-pub fn part02(data: &str) {
+pub fn part02(data: &str) -> u32 {
     let (first, second) = parse_input(data);
-    let answer: u32 = first
+    first
         .iter()
         .map(|f| second.iter().filter(|s| f == *s).count() as u32 * f)
-        .sum();
-    println!("Day 1, Part 2 answer is: {}", answer);
+        .sum()
 }
 
 /// The data is two integers per line, separated by three spaces
@@ -42,4 +44,23 @@ fn parse_input(data: &str) -> (Vec<u32>, Vec<u32>) {
         second.push(split.next().unwrap().parse::<u32>().unwrap());
     }
     (first, second)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DATA: &str = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
+
+    #[test]
+    fn test_part01() {
+        let answer = part01(DATA);
+        assert_eq!(answer, 11);
+    }
+
+    #[test]
+    fn test_part02() {
+        let answer = part02(DATA);
+        assert_eq!(answer, 31);
+    }
 }
