9f3b9577d7a2a11f729d8088f1b373a9f9d8f115
chriswmann <8133496+chriswmann@users.noreply.github.com>
2024-12-03 19:25:59 +0000

    Parse day 2 input

--- src/day02.rs
+++ src/day02.rs
@@ -1,34 +1,33 @@
 use crate::cli::Part;
 pub fn run(data: &str, part: Option<Part>) {
     if let Some(part) = part {
-        match part {
+        let answer = match part {
             Part::One => part01(data),
             Part::Two => part02(data),
-        }
+        };
+        println!("Day 2, Part {} answer is: {:?}", part, answer);
         return;
     }
-    part01(data);
-    part02(data);
+    let part01_answer = part01(data);
+
+    println!("Day 2, Part 1 answer is: {:?}", part01_answer);
+    let part02_answer = part02(data);
+    println!("Day 2, Part 2 answer is: {:?}", part02_answer);
 }
 
-pub fn part01(data: &str) {
-    let answer = "Unimplemented";
-    println!("Day 1, Part 1 answer is: {}", answer);
+pub fn part01(data: &str) -> u32 {
+    let lines = parse_input(data);
+    println!("Day 1, Part 1 data is: {:?}", &lines[..2]);
+    42
 }
 
-pub fn part02(data: &str) {
-    let answer = "Unimplemented";
-    println!("Day 1, Part 2 answer is: {}", answer);
+pub fn part02(data: &str) -> u32 {
+    let lines = parse_input(data);
+    println!("Day 1, Part 2 data is: {:?}", &lines[..2]);
+    42
 }
 
 /// The data is two integers per line, separated by three spaces
-fn parse_input(data: &str) -> (Vec<u32>, Vec<u32>) {
-    let mut first = Vec::new();
-    let mut second = Vec::new();
-    for line in data.lines() {
-        let mut split = line.split_whitespace();
-        first.push(split.next().unwrap().parse::<u32>().unwrap());
-        second.push(split.next().unwrap().parse::<u32>().unwrap());
-    }
-    (first, second)
+fn parse_input(data: &str) -> Vec<&str> {
+    data.lines().collect::<Vec<&str>>()
 }
