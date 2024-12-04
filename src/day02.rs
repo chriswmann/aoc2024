6fc911c5bd4cd962c35bbc93367280d7630a0c7b
chriswmann <8133496+chriswmann@users.noreply.github.com>
2024-12-04 20:17:52 +0000

    Day 2

--- src/day02.rs
+++ src/day02.rs
@@ -17,17 +17,109 @@ pub fn run(data: &str, part: Option<Part>) {
 
 pub fn part01(data: &str) -> u32 {
     let lines = parse_input(data);
-    println!("Day 1, Part 1 data is: {:?}", &lines[..2]);
-    42
+    let mut rows = Vec::new();
+    lines
+        .iter()
+        .for_each(|line| rows.push(line_to_digits(line)));
+    let mut answer = 0u32;
+    for row in rows {
+        if report_decreasing_slowly(&row).is_none() || report_increasing_slowly(&row).is_none() {
+            answer += 1;
+        }
+    }
+    answer
 }
 
+fn line_to_digits(line: &str) -> Vec<u32> {
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+// If the levels aren't decreasing slowly, return the index of the
+// level that is wrong (so that it can be removed for the problem dampener).
+// Otherwise return None (the report is good).
+fn report_decreasing_slowly(report: &[u32]) -> Option<usize> {
+    let len = report.len();
+    for level_idx in 0..len - 1 {
+        let left = report[level_idx];
+        let right = report[level_idx + 1];
+        if !(1..4).contains(&(left.saturating_sub(right))) {
+            return Some(level_idx);
+        }
+    }
+    None
+}
+
+// If the levels aren't increasing slowly, return the index of the
+// level that is wrong (so that it can be removed for the problem dampener).
+// Otherwise return None (the report is good).
+fn report_increasing_slowly(report: &[u32]) -> Option<usize> {
+    let len = report.len();
+    for level_idx in 0..len - 1 {
+        let left = report[level_idx];
+        let right = report[level_idx + 1];
+        if !(1..4).contains(&(right.saturating_sub(left))) {
+            return Some(level_idx);
+        }
+    }
+    None
+}
+
+// Terribly inefficient brute force solution but it works.
 pub fn part02(data: &str) -> u32 {
     let lines = parse_input(data);
-    println!("Day 1, Part 2 data is: {:?}", &lines[..2]);
-    42
+    let mut reports = Vec::new();
+    lines
+        .iter()
+        .for_each(|line| reports.push(line_to_digits(line)));
+    let mut answer = 0u32;
+    for report in reports {
+        if report_decreasing_slowly(&report).is_none()
+            || report_increasing_slowly(&report).is_none()
+        {
+            answer += 1;
+            continue;
+        }
+        for level_idx in 0..report.len() {
+            let mut new_report = report.clone();
+            new_report.remove(level_idx);
+            if report_decreasing_slowly(&new_report).is_none()
+                || report_increasing_slowly(&new_report).is_none()
+            {
+                answer += 1;
+                break;
+            }
+        }
+    }
+    answer
 }
 
 /// The data is two integers per line, separated by three spaces
 fn parse_input(data: &str) -> Vec<&str> {
     data.lines().collect::<Vec<&str>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const DATA: &str = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9";
+    #[test]
+    fn test_part01() {
+        let answer = part01(DATA);
+        assert_eq!(answer, 2);
+    }
+
+    #[test]
+    fn test_part02_given_input() {
+        let answer = part02(DATA);
+        assert_eq!(answer, 4);
+    }
+
+    #[test]
+    fn test_part02_edge_case_input() {
+        let data = "48 46 47 49 51 54 56\n1 1 2 3 4 5\n1 2 3 4 5 5\n5 1 2 3 4 5\n1 4 3 2 1\n1 6 7 8 9\n1 2 3 4 3\n9 8 7 6 7\n7 10 8 10 11\n29 28 27 25 26 25 22 20";
+        let ansewr = part02(data);
+        assert_eq!(ansewr, 10);
+    }
 }
